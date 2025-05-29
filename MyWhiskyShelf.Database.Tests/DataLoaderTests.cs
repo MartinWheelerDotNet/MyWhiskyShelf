b947326d78ca42b8cf3b26a8d108bb824b33652c
MartinWheelerDotNet <martinwheeler1980@gmail.com>
2025-05-29 17:44:14 +0100

    Initial Project Commit
    * Setup project structure
    * Add DataLoader class to load distillery data which will be used to seed the database

---
+++ MyWhiskyShelf.Database.Tests/DataLoaderTests.cs
@@ -0,0 +1,120 @@
+﻿namespace MyWhiskyShelf.Database.Tests;
+
+public class DataLoaderTests
+{
+    [Fact]
+    public async Task When_GetDistilleryData_WithFileNotFound_ExpectExceptionThrown()
+    {
+        const string filename = "./not-a-valid-file.csv";
+        var dataLoader = new DataLoader();
+
+        var exception = await Assert.ThrowsAsync<FileNotFoundException>(
+            () => dataLoader.GetDistilleryData(filename));
+        
+        Assert.Equal($"'{filename}' not found when loading distillery data.", exception.Message);
+    }
+
+    [Fact]
+    public async Task When_GetDistilleryData_WithEmptyFileProvided_ExpectExceptionThrown()
+    {
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/DistilleryData/empty-file.json");
+        
+        var dataLoader = new DataLoader();
+        var exception = await Assert.ThrowsAsync<InvalidDataException>(
+            () => dataLoader.GetDistilleryData(filePath));
+        
+        Assert.Equal($"'{filePath}' is found, but empty, when loading distillery data.", exception.Message);
+    }
+    
+    [Fact]
+    public async Task When_GetDistilleryData_WithInvalidFormatProvided_ExpectExceptionThrown()
+    {
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/DistilleryData/invalid-format.json");
+        
+        var dataLoader = new DataLoader();
+        var exception = await Assert.ThrowsAsync<InvalidDataException>(
+            () => dataLoader.GetDistilleryData(filePath));
+        
+        Assert.Equal($"'{filePath}' is found, but contains invalid data, when loading distillery data.", exception.Message);
+    }
+    
+    [Fact]
+    public async Task When_GetDistilleryData_WithNoDistilleryRecords_ExpectEmptyListOfDistilleryData()
+    {
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/DistilleryData/no-distillery-data.json");
+
+        var dataLoader = new DataLoader();
+        var distilleries = await dataLoader.GetDistilleryData(filePath);
+
+        Assert.Empty(distilleries);
+    }
+    
+    [Fact]
+    public async Task When_GetDistilleryData_WithOneDistillery_ExpectListOfJustThatDistillery()
+    {
+        var expectedDistillery = new DistilleryData
+        {
+            Distillery = "Aberargie",
+            Location = "Aberargie",
+            Region = "Lowland",
+            Founded = 2017,
+            Owner = "Perth Distilling Co",
+            DistilleryType = "Malt",
+            Active = true
+        };
+            
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/DistilleryData/single-distillery.json");
+        
+        var dataLoader = new DataLoader();
+        var distilleries = await dataLoader.GetDistilleryData(filePath);
+
+        var distillery = Assert.Single(distilleries);
+        Assert.Equal(expectedDistillery, distillery);
+    }
+    
+    [Fact]
+    public async Task When_GetDistilleryData_WithThreeDistilleries_ExpectListOfJustThoseDistilleries()
+    {
+        var expectedDistilleries = new []
+        {
+            new DistilleryData
+            {
+                Distillery = "Aberargie",
+                Location = "Aberargie",
+                Region = "Lowland",
+                Founded = 2017,
+                Owner = "Perth Distilling Co",
+                DistilleryType = "Malt",
+                Active = true
+            },
+            new DistilleryData
+            {
+                Distillery = "Aberfeldy",
+                Location = "Aberfeldy",
+                Region = "Highland",
+                Founded = 1896,
+                Owner = "John Dewar & Sons",
+                DistilleryType = "Malt",
+                Active = true
+            },
+            new DistilleryData
+            {
+                Distillery = "Aberlour",
+                Location = "Aberlour",
+                Region = "Speyside",
+                Founded = 1879,
+                Owner = "Chivas Brothers",
+                DistilleryType = "Malt",
+                Active = true
+            }
+        };
+        
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/DistilleryData/three-distilleries.json");
+        
+        var dataLoader = new DataLoader();
+        var distilleries = await dataLoader.GetDistilleryData(filePath);
+
+        Assert.Equal(3, expectedDistilleries.Length);
+        Assert.All(distilleries, distillery => Assert.Contains(distillery, expectedDistilleries));
+    }
+}
---
+++ MyWhiskyShelf.Database/DataLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace MyWhiskyShelf.Database;
+
+public class DataLoader
+{
+    private const string DistilleryPrefix = "when loading distillery data.";
+
+    public async Task<List<DistilleryData>> GetDistilleryData(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"'{filePath}' not found {DistilleryPrefix}");
+        }
+
+        if (new FileInfo(filePath).Length == 0)
+        {
+            throw new InvalidDataException($"'{filePath}' is found, but empty, {DistilleryPrefix}");    
+        }
+
+        try
+        {
+            await using var fileStream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<List<DistilleryData>>(fileStream) ?? [];
+
+        }
+        catch (JsonException)
+        {
+            throw new InvalidDataException($"'{filePath}' is found, but contains invalid data, {DistilleryPrefix}");
+        }
+    } 
+}
+
+[Serializable]
+public record DistilleryData
+{
+    public required string Distillery { get; init; }
+    public required string Location { get; init; }
+    public required string Region  { get; init; }
+    public required int Founded  { get; init; }
+    public required string Owner  { get; init; }
+    public required string DistilleryType  { get; init; }
+    public required bool Active  { get; init; }
+}
+    
