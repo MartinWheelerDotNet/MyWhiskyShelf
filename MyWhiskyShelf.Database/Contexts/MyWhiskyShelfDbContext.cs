13e8f4b994381c074bc80c22389ed2a74df357d5
Martin Wheeler <martinwheeler1980@gmail.com>
2025-05-31 22:50:20 +0100

    Refactor MyWhiskyShelfDbContext (#5)
    * Refactor MyWhiskyShelfDbContext
    
    * Refactor to ensure that the `DbContext` stays focused on entities and EF mechanics.
    * `DistilleryReadService` added to handle units of work against the `DbContext`.
    * Copy across test to run against the service instead of the context.
    
    * * Add `DistilleryReadService` to the DI Container.

--- MyWhiskyShelf.Database/Contexts/MyWhiskyShelfDbContext.cs
+++ MyWhiskyShelf.Database/Contexts/MyWhiskyShelfDbContext.cs
@@ -1,16 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using MyWhiskyShelf.Database.Extensions;
 using MyWhiskyShelf.Database.Models;
-using MyWhiskyShelf.Models;
 
 namespace MyWhiskyShelf.Database.Contexts;
 
 public class MyWhiskyShelfDbContext(DbContextOptions<MyWhiskyShelfDbContext> options) : DbContext(options)
 {
-    private DbSet<DistilleryEntity> Distilleries { get; set; }
+    internal DbSet<DistilleryEntity> Distilleries { get; set; }
 
-    public async Task<List<Distillery>> GetAllDistilleriesAsync()
-        => await Distilleries
-            .Select(distilleryEntity => distilleryEntity.ProjectToDistillery())
-            .ToListAsync();
+   
 }
