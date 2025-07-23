c3dea2064272335d90d72a8a3546909c2847e7a2
MartinWheelerDotNet <martinwheeler1980@gmail.com>
2025-07-23 22:56:55 +0100

    Update WebApi WhiskyBottle add endpoint
    * Update WebApi WhiskyBottle add endpoint to handle problems occuring.
    * Refactor tests into separate files for each endpoint type.
    * Add integration tests for WebApi Add Whisky Bottle.

--- MyWhiskyShelf.IntegrationTests/WebApi/WebApiDistilleriesTests.cs
+++ MyWhiskyShelf.IntegrationTests/WebApi/WebApiDistilleriesTests.cs
@@ -3,12 +3,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyWhiskyShelf.Core.Models;
 using MyWhiskyShelf.IntegrationTests.Fixtures;
-using MyWhiskyShelf.TestHelpers.Data;
 using DistilleryTestData = MyWhiskyShelf.TestHelpers.Data.DistilleryTestData;
 
 namespace MyWhiskyShelf.IntegrationTests.WebApi;
 
-public static class WebApiTests
+public static class WebApiDistilleriesTests
 {
     private const string WebApiResourceName = "WebApi";
 
@@ -211,19 +210,6 @@ public static class WebApiTests
 
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
             Assert.Equal("/distilleries/Burn%20O%27Bennie", response.Headers.Location!.ToString());
-        }
-
-        [Fact]
-        public async Task When_AddWhiskyBottle_Expect_CreatedWithLocationHeaderSet()
-        {
-            const string endpoint = "/whiskyBottle/add";
-
-            using var httpClient = fixture.Application.CreateHttpClient(WebApiResourceName);
-            var response = await httpClient.PostAsJsonAsync(endpoint, WhiskyBottleTestData.AllValuesPopulated);
-
-            Assert.Multiple(
-                () => Assert.Equal(HttpStatusCode.Created, response.StatusCode),
-                () => Assert.Equal("/whiskyBottle/All%20Values%20Populated", response.Headers.Location!.ToString()));
         }
     }
 
---
+++ MyWhiskyShelf.IntegrationTests/WebApi/WebApiWhiskyBottleTests.cs
@@ -0,0 +1,61 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using MyWhiskyShelf.IntegrationTests.Fixtures;
+using MyWhiskyShelf.TestHelpers.Data;
+
+namespace MyWhiskyShelf.IntegrationTests.WebApi;
+
+[Collection("AspireTests")]
+public class WebApiWhiskyBottleTests(MyWhiskyShelfBaseFixtureEmptyDb fixture)
+    : IClassFixture<MyWhiskyShelfBaseFixtureEmptyDb>
+{
+    private const string WebApiResourceName = "WebApi";
+    
+    [Fact]
+    public async Task When_AddWhiskyBottle_Expect_CreatedWithLocationHeaderSet()
+    {
+        const string endpoint = "/whiskyBottle/add";
+
+        using var httpClient = fixture.Application.CreateHttpClient(WebApiResourceName);
+        var response = await httpClient.PostAsJsonAsync(endpoint, WhiskyBottleTestData.AllValuesPopulated);
+
+        Assert.Multiple(
+            () => Assert.Equal(HttpStatusCode.Created, response.StatusCode),
+            () => Assert.Equal("/whiskyBottle/All%20Values%20Populated", response.Headers.Location!.ToString()));
+    }
+
+    [Fact]
+    public async Task When_AddWhiskyBottleAndBottleCannotBeAddedToDatabase_Expect_ValidationProblemDetails()
+    {
+        const string endpoint = "/whiskyBottle/add";
+        
+        // we are deliberately breaking the model constraints here to so that the database will attempt to insert
+        // an invalid entity, which will cause it to fail.
+        var whiskyBottleWithoutName = WhiskyBottleTestData.AllValuesPopulated with { Name = null! };
+        
+        var expectedValidationProblem = new ValidationProblemDetails
+        {
+            Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+            Title = "One or more validation errors occurred.",
+            Status = 400,
+            Instance = null,
+            Errors = new Dictionary<string, string[]>
+            {
+                ["WhiskyBottle"] =
+                [
+                    """
+                    An error occurred trying to add the whisky bottle to the database.
+                    Ensure all required fields have been set.
+                    """
+                ]
+            }
+        };
+
+        using var httpClient = fixture.Application.CreateHttpClient(WebApiResourceName);
+        var response = await httpClient.PostAsJsonAsync(endpoint, whiskyBottleWithoutName);
+        var body = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.Equivalent(expectedValidationProblem, body);
+    }
+}
