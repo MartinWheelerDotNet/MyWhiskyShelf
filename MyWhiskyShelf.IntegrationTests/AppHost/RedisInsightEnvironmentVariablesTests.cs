951403a8f1ec7b386dd4b18a1e9c2159f9e7bc62
Martin Wheeler <martinwheeler1980@gmail.com>
2025-09-10 00:05:19 +0100

    * Fix mutation testing running against migrations (#78)
    * Add missing mutation tests for environment variables

---
+++ MyWhiskyShelf.IntegrationTests/AppHost/RedisInsightEnvironmentVariablesTests.cs
@@ -0,0 +1,43 @@
+using Aspire.Hosting.Redis;
+using Projects;
+
+namespace MyWhiskyShelf.IntegrationTests.AppHost;
+
+public class RedisInsightEnvironmentVariablesTests
+{
+    [Fact]
+    public async Task When_RedisInsightIsEnabledInDevelopment_Expect_RedisInsightContainerIsPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Development",
+                "MYWHISKYSHELF_REDIS_INSIGHT_ENABLED=true"
+            ]);
+
+        Assert.NotEmpty(appHostBuilder.Resources.OfType<RedisInsightResource>());
+    }
+
+    [Fact]
+    public async Task When_RedisInsightIsDisabledInDevelopment_Expect_RedisInsightContainerIsNotPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Development",
+                "MYWHISKYSHELF_REDIS_INSIGHT_ENABLED=false"
+            ]);
+
+        Assert.Empty(appHostBuilder.Resources.OfType<RedisInsightResource>());
+    }
+
+    [Fact]
+    public async Task When_RedisInsightIsEnabledInProduction_Expect_RedisInsightContainerIsNotPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Production",
+                "MYWHISKYSHELF_PG_WEB_ENABLED=true"
+            ]);
+
+        Assert.Empty(appHostBuilder.Resources.OfType<RedisInsightResource>());
+    }
+}
