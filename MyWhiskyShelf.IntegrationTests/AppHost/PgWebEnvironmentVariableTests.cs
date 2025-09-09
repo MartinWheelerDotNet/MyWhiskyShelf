d5390563c4e366ef39d7addb6a0e5d4ec9a58df9
MartinWheelerDotNet <martinwheeler1980@gmail.com>
2025-09-10 00:00:13 +0100

    * Fix mutation testing running against migrations * Add missing mutation tests for environment variables

---
+++ MyWhiskyShelf.IntegrationTests/AppHost/PgWebEnvironmentVariableTests.cs
@@ -0,0 +1,45 @@
+using Aspire.Hosting.Postgres;
+using Projects;
+
+namespace MyWhiskyShelf.IntegrationTests.AppHost;
+
+public class PgWebEnvironmentVariableTests
+{
+    [Fact]
+    public async Task When_PgWebIsEnabledInDevelopment_Expect_PgWebContainerIsPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Development",
+                "MYWHISKYSHELF_PG_WEB_ENABLED=true"
+            ]);
+
+        var pgWebContainer = appHostBuilder.Resources.OfType<PgWebContainerResource>().SingleOrDefault();
+
+        Assert.NotNull(pgWebContainer?.PrimaryEndpoint);
+    }
+
+    [Fact]
+    public async Task When_PgWebIsDisabledInDevelopment_Expect_PgWebContainerIsNotPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Development",
+                "MYWHISKYSHELF_PG_WEB_ENABLED=false"
+            ]);
+
+        Assert.Empty(appHostBuilder.Resources.OfType<PgWebContainerResource>());
+    }
+
+    [Fact]
+    public async Task When_PgWebIsEnabledInProduction_Expect_PgWebContainerIsNotPresent()
+    {
+        await using var appHostBuilder = await DistributedApplicationTestingBuilder
+            .CreateAsync<MyWhiskyShelf_AppHost>([
+                "--environment=Production",
+                "MYWHISKYSHELF_PG_WEB_ENABLED=true"
+            ]);
+
+        Assert.Empty(appHostBuilder.Resources.OfType<PgWebContainerResource>());
+    }
+}
