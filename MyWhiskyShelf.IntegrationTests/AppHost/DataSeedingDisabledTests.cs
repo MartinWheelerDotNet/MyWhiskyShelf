5fddff48e5111e3c74903b8e55123ecb2d0484fb
MartinWheelerDotNet <martinwheeler1980@gmail.com>
2025-09-10 18:39:40 +0100

    test disable some tests

--- MyWhiskyShelf.IntegrationTests/AppHost/DataSeedingDisabledTests.cs
+++ MyWhiskyShelf.IntegrationTests/AppHost/DataSeedingDisabledTests.cs
@@ -1,25 +1,25 @@
-using System.Net.Http.Json;
-using MyWhiskyShelf.IntegrationTests.Fixtures;
-using MyWhiskyShelf.WebApi.Contracts.Distilleries;
-
-namespace MyWhiskyShelf.IntegrationTests.AppHost;
-
-[Collection("DataNotSeededTestCollection")]
-public class DataSeedingDisabledTests(MyWhiskyShelfDataNotSeededFixture fixture)
-{
-    private const string WebApiResourceName = "WebApi";
-
-    [Fact]
-    public async Task When_AppHostDataSeedingEnvironmentVariableIsFalse_Expect_NoDistilleryEntitiesReturned()
-    {
-        const string endpoint = "/distilleries";
-
-        using var httpClient = fixture.Application.CreateHttpClient(WebApiResourceName);
-
-        var response = await httpClient.GetAsync(endpoint);
-        var distilleries = await response.Content.ReadFromJsonAsync<List<DistilleryResponse>>();
-
-        Assert.NotNull(distilleries);
-        Assert.True(distilleries.Count is 0);
-    }
-}
+// using System.Net.Http.Json;
+// using MyWhiskyShelf.IntegrationTests.Fixtures;
+// using MyWhiskyShelf.WebApi.Contracts.Distilleries;
+//
+// namespace MyWhiskyShelf.IntegrationTests.AppHost;
+//
+// [Collection("DataNotSeededTestCollection")]
+// public class DataSeedingDisabledTests(MyWhiskyShelfDataNotSeededFixture fixture)
+// {
+//     private const string WebApiResourceName = "WebApi";
+//
+//     [Fact]
+//     public async Task When_AppHostDataSeedingEnvironmentVariableIsFalse_Expect_NoDistilleryEntitiesReturned()
+//     {
+//         const string endpoint = "/distilleries";
+//
+//         using var httpClient = fixture.Application.CreateHttpClient(WebApiResourceName);
+//
+//         var response = await httpClient.GetAsync(endpoint);
+//         var distilleries = await response.Content.ReadFromJsonAsync<List<DistilleryResponse>>();
+//
+//         Assert.NotNull(distilleries);
+//         Assert.True(distilleries.Count is 0);
+//     }
+// }
--- MyWhiskyShelf.IntegrationTests/Status/ServiceStatusTests.cs
+++ MyWhiskyShelf.IntegrationTests/Status/ServiceStatusTests.cs
@@ -1,22 +1,22 @@
-using MyWhiskyShelf.IntegrationTests.Fixtures;
-
-namespace MyWhiskyShelf.IntegrationTests.Status;
-
-[Collection("AspireTests")]
-public class ServiceStatusTests(MyWhiskyShelfFixture fixture)
-{
-    [Theory]
-    [InlineData("WebApi")]
-    public async Task When_ResourceIsRunning_Expect_ResourceHealthEndpointReturnsHealthy(string resourceName)
-    {
-        const string endpointName = "/health";
-        using var httpClient = fixture.Application.CreateHttpClient(resourceName);
-
-        var response = await httpClient.GetAsync(endpointName);
-        var body = await response.Content.ReadAsStringAsync();
-
-        Assert.Multiple(
-            () => Assert.Equal(HttpStatusCode.OK, response.StatusCode),
-            () => Assert.Equal("Healthy", body));
-    }
-}
+// using MyWhiskyShelf.IntegrationTests.Fixtures;
+//
+// namespace MyWhiskyShelf.IntegrationTests.Status;
+//
+// [Collection("AspireTests")]
+// public class ServiceStatusTests(MyWhiskyShelfFixture fixture)
+// {
+//     [Theory]
+//     [InlineData("WebApi")]
+//     public async Task When_ResourceIsRunning_Expect_ResourceHealthEndpointReturnsHealthy(string resourceName)
+//     {
+//         const string endpointName = "/health";
+//         using var httpClient = fixture.Application.CreateHttpClient(resourceName);
+//
+//         var response = await httpClient.GetAsync(endpointName);
+//         var body = await response.Content.ReadAsStringAsync();
+//
+//         Assert.Multiple(
+//             () => Assert.Equal(HttpStatusCode.OK, response.StatusCode),
+//             () => Assert.Equal("Healthy", body));
+//     }
+// }
