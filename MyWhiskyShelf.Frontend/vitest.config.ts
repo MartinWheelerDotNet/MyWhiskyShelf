6e36f4ff4e966651ee6a2a1ab8e3899701d7fed1
MartinWheelerDotNet <martinwheeler1980@gmail.com>
2025-10-04 15:57:27 +0100

    * Fix Vitest coverage missing dependency issue.

--- MyWhiskyShelf.Frontend/vitest.config.ts
+++ MyWhiskyShelf.Frontend/vitest.config.ts
@@ -6,6 +6,15 @@ export default defineConfig({
         environment: "jsdom",
         setupFiles: ["/src/test/setup.ts"],
         globals: true,
+        reporters: ["default", "junit"],
+        outputFile: "./TestResults/vitest-junit.xml",
+        coverage: {
+            provider: "v8",
+            reporter: ["lcov", "html", "text-summary"],
+            reportsDirectory: "coverage",
+            include: ["src/**/*.{ts,tsx}"],
+            exclude: ["**/*.test.*", "**/__tests__/**", "test/**"],
+        },
     },
     resolve: {
         alias: {
